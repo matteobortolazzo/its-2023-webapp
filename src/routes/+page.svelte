0c5060890fb701d2a085fee7a8bc61cdf30594b5
Matteo Bortolazzo <matteobortolazzo@pm.me>
2024-05-16 12:11:34 +0200

    add error

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,9 @@
+<script>
+	let;
+</script>
+
 <h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+/h1>
+<p>
+	Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation
+</p>
